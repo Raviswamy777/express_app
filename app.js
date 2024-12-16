82295cca9cadbf441247a1d722e9ef54eef32d91
raviswamy777 <raviswamy@i.camp>
2024-12-16 13:12:17 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import "dotenv/config"
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World! From '+ process.env.USER_NAME);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
